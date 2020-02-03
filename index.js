8df712b24a53bd74af4cbb0e2a6e0e9b994c7fa7
ceboulba <antoine@archipicture.fr>
2020-02-03 23:40:46 +0100

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1580715376/ca-jussey/cam10.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580715596/ca-jussey/cam09.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580719923/ca-jussey/cam16.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1580733315/ca-jussey/cam05.jpg',
+  'https://res.cloudinary.com/archipicture/image/upload/v1580769628/ca-jussey/cam05.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580736995/ca-jussey/cam18.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580720640/ca-jussey/cam11.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580721036/ca-jussey/cam20.jpg',
