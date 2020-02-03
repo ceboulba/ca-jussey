b30ce2919240358a17fb8eef3d87dc0791a4eb9c
ceboulba <antoine@archipicture.fr>
2020-02-03 09:49:27 +0100

    2 files modified

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1580715376/ca-jussey/cam10.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580708513/ca-jussey/cam05.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580715596/ca-jussey/cam09.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1574809392/relyens-olivet/relyens-olivet-05.jpg',
 ]
 const imgName= [
   'Vue axo 01',
