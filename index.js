2970c6a74d2796b3dcb0e16f004fe18b4534a21c
ceboulba <antoine@archipicture.fr>
2020-02-04 06:40:09 +0100

    1 file modified

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1574761687/relyens-olivet/relyens-olivet-01.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580765411/ca-jussey/cam01.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580767129/ca-jussey/cam08.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1580715376/ca-jussey/cam10.jpg',
+  'https://res.cloudinary.com/archipicture/image/upload/v1580794790/ca-jussey/cam10.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580769775/ca-jussey/cam09.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580778363/ca-jussey/cam16.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1580769628/ca-jussey/cam05.jpg',
